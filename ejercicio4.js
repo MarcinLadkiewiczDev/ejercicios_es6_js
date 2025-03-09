0bae11a872bb32a4cf4282fadbf67f3e723cbd05
Marcin Ladkiewicz <marcin.ladkiewicz@gmail.com>
2025-03-09 20:06:51 +0100

    Update ejercicio4.js

--- ejercicio4.js
+++ ejercicio4.js
@@ -5,3 +5,19 @@ const ages = [22, 14, 24, 55, 65, 21, 12, 13, 90];
 
 const filteredAges = ages.filter(age => age > 18);
 console.log(filteredAges);
+
+/* 4.2 Dado el siguiente array, utiliza .filter() para generar un nuevo array 
+con los valores que sean par. */
+
+const agesPair = ages.filter(age => age % 2 == 0);
+console.log(agesPair);
+
+/* 4.3 Dado el siguiente array, utiliza .filter() para generar un nuevo array 
+con los streamers que tengan el gameMorePlayed = 'League of Legends'. */
+
+const streamers = [
+  { name: "Rubius", age: 32, gameMorePlayed: "Minecraft" },
+  { name: "Ibai", age: 25, gameMorePlayed: "League of Legends" },
+  { name: "Reven", age: 43, gameMorePlayed: "League of Legends" },
+  { name: "AuronPlay", age: 33, gameMorePlayed: "Among Us" },
+];
