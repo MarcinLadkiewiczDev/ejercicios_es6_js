51c7ad327b0c19f38c7f5d20b3983495a5a5a14e
Marcin Ladkiewicz <marcin.ladkiewicz@gmail.com>
2025-03-09 19:23:57 +0100

    Primer ejercicio

---
+++ ejercicio2.js
@@ -0,0 +1,11 @@
+/* 2.1 Dado el siguiente array, crea una copia usando spread operators. */
+
+const pointsList = [32, 54, 21, 64, 75, 43];
+
+const pointsListCopy = [...pointsList];
+console.log(pointsList);
+console.log(pointsListCopy);
+
+/* 2.2 Dado el siguiente objeto, crea una copia usando spread operators. */
+
+const toy = { name: "Bus laiyiar", date: "20-30-1995", color: "multicolor" };
