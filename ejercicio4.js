f406ac0a7df0224b8480f7300d6f5802d8bb2857
Marcin Ladkiewicz <marcin.ladkiewicz@gmail.com>
2025-03-09 20:36:30 +0100

    Update ejercicio4.js

--- ejercicio4.js
+++ ejercicio4.js
@@ -32,3 +32,14 @@ usar la funcion .includes() para la comprobación. */
 
 const streamersWithU = streamers.filter(streamer => streamer.name.includes('u'));
 console.log(streamersWithU);
+
+/* 4.5 utiliza .filter() para generar un nuevo array con los streamers que incluyan 
+el caracter 'Legends' en su propiedad .gameMorePlayed. Recomendamos usar la funcion 
+.includes() para la comprobación.
+Además, pon el valor de la propiedad .gameMorePlayed a MAYUSCULAS cuando 
+.age sea mayor que 35. */
+
+const streamersLegendsAge = streamers.filter(streamer => streamer.gameMorePlayed.includes("Legends"))
+    .map(streamer => ({...streamer, gameMorePlayed: streamer.age > 35 ? streamer.gameMorePlayed.toUpperCase() : streamer.gameMorePlayed}));
+
+console.log(streamersLegendsAge);
